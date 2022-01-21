fb910c7d000a4b64a7fc8e4d07bf73d7bbd00d7e
JoseSoteloGithub <josegsotelo@yahoo.com>
2022-01-20 18:43:57 -0700

    Update win32comExcelTools.py

--- win32comExcelTools.py
+++ win32comExcelTools.py
@@ -1,19 +1,19 @@
-def find_nth(haystack, needle, n):
-    start = haystack.find(needle)
-    while start >= 0 and n > 1:
-        start = haystack.find(needle, start+len(needle))
-        n -= 1
+def find_nth(haystack_str, needle_str, n_int):
+    start = haystack_str.find(needle_str)
+    while start >= 0 and n_int > 1:
+        start = haystack_str.find(needle_str, start+len(needle_str))
+        n_int -= 1
     return start
-def get_column_letter(worksheet, substring, header_row, worksheet_column_letters_dict={}):
+def get_column_letter(worksheet_obj, substring_str, header_row_int, worksheet_column_letters_dict={}):
 # Returns column letter or -1 if not found
     print('worksheet_column_letters_dict:', worksheet_column_letters_dict)
     try:
-        column_letter = worksheet_column_letters_dict[substring]
+        column_letter = worksheet_column_letters_dict[substring_str]
         print('Returned from try: on worksheet_column_letters_dict')
         return column_letter
     except:
-        last_column_letter = get_last_column_letter(worksheet, header_row)
-        column_header = worksheet.Range('A' + str(header_row) + ':' + last_column_letter + str(header_row)).Find(What=substring, SearchOrder=constants.xlByRows, 
+        last_column_letter = get_last_column_letter(worksheet_obj, header_row_int)
+        column_header = worksheet_obj.Range('A' + str(header_row_int) + ':' + last_column_letter + str(header_row_int)).Find(What=substring_str, SearchOrder=constants.xlByRows, 
                                                         SearchDirection=constants.xlNext)
 
         #print('column_header:', column_header)
@@ -23,14 +23,14 @@ def get_column_letter(worksheet, substring, header_row, worksheet_column_letters
             column_letter = column_header.Address[first_dolla + 1:second_dolla]
         else:
             column_letter = -1
-        #print(f'{worksheet.Parent.Name}.{worksheet.Name}.{substring}: ', column_letter)
+        #print(f'{worksheet_obj.Parent.Name}.{worksheet_obj.Name}.{substring_str}: ', column_letter)
         return column_letter
-def get_last_column_letter(worksheet, header_row=0):
+def get_last_column_letter(worksheet_ojb, header_row_int=0):
 # Returns last column letter
-    print('header_row:', header_row)
-    if header_row != 0:
-        last_column_letter_address = worksheet.Rows(header_row).Find(What='*', SearchOrder=constants.xlByColumns, SearchDirection=constants.xlPrevious).Address
+    print('header_row_int:', header_row_int)
+    if header_row_int != 0:
+        last_column_letter_address = worksheet_obj.Rows(header_row_int).Find(What='*', SearchOrder=constants.xlByColumns, SearchDirection=constants.xlPrevious).Address
     else:
-        last_column_letter_address = worksheet.Cells.Find(What='*', SearchOrder=constants.xlByColumns, SearchDirection=constants.xlPrevious).Address
+        last_column_letter_address = worksheet_obj.Cells.Find(What='*', SearchOrder=constants.xlByColumns, SearchDirection=constants.xlPrevious).Address
     last_column_letter = last_column_letter_address.split('$')[1]
     return(last_column_letter)
