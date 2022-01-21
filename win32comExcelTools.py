788995511db6c9fac7091b2900eb99dbd1107d1c
JoseSoteloGithub <josegsotelo@yahoo.com>
2022-01-20 18:55:19 -0700

    Update win32comExcelTools.py

--- win32comExcelTools.py
+++ win32comExcelTools.py
@@ -1,5 +1,5 @@
 def find_nth(haystack_str, needle_str, n_int):
-# Returns index of nth needle in haystack    
+# Returns index of nth needle in haystack
     start = haystack_str.find(needle_str)
     while start >= 0 and n_int > 1:
         start = haystack_str.find(needle_str, start+len(needle_str))
@@ -35,3 +35,23 @@ def get_last_column_letter(worksheet_ojb, header_row_int=0):
         last_column_letter_address = worksheet_obj.Cells.Find(What='*', SearchOrder=constants.xlByColumns, SearchDirection=constants.xlPrevious).Address
     last_column_letter = last_column_letter_address.split('$')[1]
     return(last_column_letter)
+def get_last_row(worksheet_obj):
+# Returns last row as int
+    return(worksheet_obj.Cells.Find(What='*', SearchOrder=constants.xlByRows, SearchDirection=constants.xlPrevious).Row)
+def get_header_row(worksheet_obj, search_string_str):
+# returns header row as int, -1 if not found
+    header_range = worksheet_obj.Rows.Find(What=search_string_str, SearchOrder=constants.xlByRows, 
+                                                        SearchDirection=constants.xlNext)
+    if not header_range is None:
+        header_row = header_range.Row
+    else:
+        header_row = -1
+    header_row = str(header_row)
+    return header_row
+def get_list_column_headers(range_to_get_obj):
+# Returns list of column headers in range
+    this_list = []
+    cols = range_to_get_obj
+    for cell in cols:
+        this_list.append(cell.Value)  
+    return this_list
