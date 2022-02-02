0c7da84f1eed86cf27c7eb2e85666e69759abe9e
JoseSoteloGithub <josegsotelo@yahoo.com>
2022-02-02 10:03:47 -0700

    Update win32comExcelTools.py

--- win32comExcelTools.py
+++ win32comExcelTools.py
@@ -7,7 +7,6 @@ def find_nth(haystack_str, needle_str, n_int):
     return start
 def get_column_letter(worksheet_obj, substring_str, header_row_int, worksheet_column_letters_dict={}):
 # Returns column letter or -1 if not found
-    print('worksheet_column_letters_dict:', worksheet_column_letters_dict)
     try:
         column_letter = worksheet_column_letters_dict[substring_str]
         print('Returned from try: on worksheet_column_letters_dict')
@@ -17,24 +16,26 @@ def get_column_letter(worksheet_obj, substring_str, header_row_int, worksheet_co
         column_header = worksheet_obj.Range('A' + str(header_row_int) + ':' + last_column_letter + str(header_row_int)).Find(What=substring_str, SearchOrder=constants.xlByRows, 
                                                         SearchDirection=constants.xlNext)
 
-        #print('column_header:', column_header)
         if not column_header is None:
             first_dolla = find_nth(column_header.Address, '$', 1)
             second_dolla = find_nth(column_header.Address, '$', 2)
             column_letter = column_header.Address[first_dolla + 1:second_dolla]
         else:
             column_letter = -1
-        #print(f'{worksheet_obj.Parent.Name}.{worksheet_obj.Name}.{substring_str}: ', column_letter)
+
         return column_letter
-def get_last_column_letter(worksheet_ojb, header_row_int=0):
+    
+def get_last_column_letter(worksheet_obj, header_row_int=0):
 # Returns last column letter
-    print('header_row_int:', header_row_int)
     if header_row_int != 0:
-        last_column_letter_address = worksheet_obj.Rows(header_row_int).Find(What='*', SearchOrder=constants.xlByColumns, SearchDirection=constants.xlPrevious).Address
+        last_column_letter_range = worksheet_obj.Rows(header_row_int).Find(What='*', SearchOrder=constants.xlByColumns, SearchDirection=constants.xlPrevious)
     else:
-        last_column_letter_address = worksheet_obj.Cells.Find(What='*', SearchOrder=constants.xlByColumns, SearchDirection=constants.xlPrevious).Address
+        last_column_letter_range = worksheet_obj.Cells.Find(What='*', SearchOrder=constants.xlByColumns, SearchDirection=constants.xlPrevious)
+    if last_column_letter_range is not None:
+        last_column_letter_address = last_column_letter_range.Address
     last_column_letter = last_column_letter_address.split('$')[1]
     return(last_column_letter)
+
 def get_last_row(worksheet_obj):
 # Returns last row as int
     return(worksheet_obj.Cells.Find(What='*', SearchOrder=constants.xlByRows, SearchDirection=constants.xlPrevious).Row)
